8d2e7367ea1f434714f0548f070ade8c8d9034a1
mostafa <mjafari1991.mj@gmail.com>
2023-08-01 16:54:09 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1fc10ef15e041c5d3c54042e52eb0c54cb9b710c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
